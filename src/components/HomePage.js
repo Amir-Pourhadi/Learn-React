43e9ccf64aff26ba7987ace66d32bdff66979884
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-12 19:47:21 +0430

    updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,10 @@
 export const HomePage = () => {
 	return (
-		<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center">
-			<h1>Welcome to IMD!</h1>
-		</div>
+		<>
+			<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center">
+				<h1>Welcome to IMD!</h1>
+			</div>
+			<strong>Here you can learn coding EZ!</strong>
+		</>
 	);
 };
