dbda3b1da9eaa1c5871642cda73e5d1afd1a410f
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-13 18:31:10 +0430

    courses added to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
 		<nav>
-			<a href="/">Home</a> | <a href="/about">About</a>
+			<a href="/">Home</a> | <a href="/about">About</a> | <a href="/courses">Courses</a>
 		</nav>
 	);
 };
