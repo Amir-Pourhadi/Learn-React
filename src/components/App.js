66a9d547aac3701e8c923df11ab1626c4e87e7b5
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-13 18:25:53 +0430

    style edited

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ export const App = () => {
 	const route = window.location.pathname;
 
 	return (
-		<>
+		<main className="container-fluid">
 			<Header />
 			{route === "/about" ? <AboutPage /> : <HomePage />}
-		</>
+		</main>
 	);
 };
