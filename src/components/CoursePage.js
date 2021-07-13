e7c16e92d2a77d03bad93824ab0f34b61d9fdd09
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-13 19:21:12 +0430

    table of courses added

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getCourses } from "../api/courseApi";
 
 export const CoursePage = () => {
 	const [courses, setCourses] = useState([]);
+	useEffect(() => {
+		getCourses().then((_courses) => setCourses(_courses));
+	}, []);
 
 	return (
-		<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center">
-			<h1>Course Page</h1>
-		</div>
+		<main>
+			<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center">
+				<h1>Course Page</h1>
+			</div>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Category</th>
+						<th>Slug</th>
+					</tr>
+				</thead>
+				<tbody>
+					{courses.map(({ id, title, slug, authorId, category }) => (
+						<tr key={id}>
+							<td>{title}</td>
+							<td>{category}</td>
+							<td>{slug}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</main>
 	);
 };
