47eb35675515bdc664f13c9053405a889375e186
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-13 18:36:59 +0430

    updated

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,7 +1,7 @@
 export const CoursePage = () => {
 	return (
-		<>
-			<h2>Courses</h2>
-		</>
-	)
-}
+		<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center">
+			<h1>Course Page</h1>
+		</div>
+	);
+};
