fab4a99b23a777f01495f6556210517f0dda5b11
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-15 23:54:46 +0430

    component created

---
+++ src/components/ManageCoursePage.jsx
@@ -0,0 +1,9 @@
+export const ManageCoursePage = () => {
+	return (
+		<>
+			<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center mx-auto">
+				<h1>Manage Course</h1>
+			</div>
+		</>
+	);
+};
