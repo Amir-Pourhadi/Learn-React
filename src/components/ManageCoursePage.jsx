f1e76bc18da52cc2c3c93ed65f0551ba3857af87
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-16 22:37:08 +0430

    CourseForm rendered

--- src/components/ManageCoursePage.jsx
+++ src/components/ManageCoursePage.jsx
@@ -1,9 +1,11 @@
+import { CourseForm } from "./CourseForm";
 export const ManageCoursePage = () => {
 	return (
 		<>
 			<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center mx-auto">
 				<h1>Manage Course</h1>
 			</div>
+			<CourseForm />
 		</>
 	);
 };
