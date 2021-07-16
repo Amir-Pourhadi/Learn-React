d018f854cd118f4914177df8e250362139ef09d8
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-16 21:31:14 +0430

    add new course btn

--- src/components/CoursePage.jsx
+++ src/components/CoursePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getCourses } from "../api/courseApi";
 import { CourseList } from "./CourseList";
 
@@ -13,6 +14,9 @@ export const CoursePage = () => {
 			<div className="bg-secondary w-25 p-3 m-3 rounded-pill text-center mx-auto">
 				<h1>Course Page</h1>
 			</div>
+			<Link to="/course" className="btn btn-primary rounded-3">
+				Add a Course
+			</Link>
 			<CourseList courses={courses} />
 		</main>
 	);
